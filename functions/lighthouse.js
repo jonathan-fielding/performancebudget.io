4ba378741b653f78aefd6b49999bba4d8cbb504c
Jonathan Fielding <j.fielding@me.com>
2019-09-13 22:35:52 +0100

    test

---
+++ functions/lighthouse.js
@@ -0,0 +1,30 @@
+// index.js
+
+const createLighthouse = require('lighthouse-lambda')
+
+exports.handler = function (event, context, callback) {
+  Promise.resolve()
+    .then(() => createLighthouse('https://example.com', { logLevel: 'info' }))
+    .then(({ chrome, start }) => {
+      return start()
+        .then((results) => {
+          return chrome.kill().then(() => callback(null, {
+            statusCode: 200,
+            body: "Hello, World"
+          }));
+        })
+        .catch((error) => {
+          // Handle errors when running Lighthouse
+          return chrome.kill().then(() => callback(error))
+        })
+    })
+    // Handle other errors
+    .catch(callback)
+}
+
+// exports.handler = function(event, context, callback) {
+//   callback(null, {
+//     statusCode: 200,
+//     body: "Hello, World"
+//   });
+// };
