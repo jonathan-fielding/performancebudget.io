81339e41e01c6d9eb73bb2fed97a8a584acea207
Jonathan Fielding <j.fielding@me.com>
2019-09-17 19:20:07 +0100

    feat: working perf budgets

---
+++ src/utils/download-budget.js
@@ -0,0 +1,13 @@
+import { saveAs } from 'file-saver';
+import buildJson from './build-json';
+
+export default function downloadBudget(budget) {
+  const content = JSON.stringify(buildJson(budget));
+  const filename = "budget.json";
+
+  const blob = new Blob([content], {
+    type: "text/plain;charset=utf-8"
+  });
+
+  saveAs(blob, filename);
+}
