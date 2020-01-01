1b3e1ea6c622ea6c99b06201f3a79c7d09f4b684
Jonathan Fielding <jonathan@Jonathans-MacBook-Pro.local>
2020-01-01 21:21:25 +0000

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://4858aba3214c4b3c82124dff3708f33c@sentry.io/1869831"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
