eebfa0a098c359321627f168b05c49a130a89c9b
Jonathan Fielding <j.fielding@me.com>
2022-11-20 21:23:31 +0000

    fix: linting

--- components/compounds/card/index.tsx
+++ components/compounds/card/index.tsx
@@ -5,19 +5,23 @@ const navigation = [
   { name: 'Performance Resources', href: '/resources' },
 ]
 
-export default function Card({
-  title, description, author, path
-}) {
+interface CardProps {
+  title: string;
+  description: string;
+  author: string;
+  path: string;
+}
+
+export default function Card({ title, description, author, path }: CardProps) {
   return (
-    <a href={path} className="p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700">
+    <a
+      href={path}
+      className="p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700"
+    >
       <h2 className="font-semibold text-3xl mb-5">{title}</h2>
-      <p>
-          {description}
-      </p>
+      <p>{description}</p>
       <hr className="my-6 border-gray-300" />
-      <p>
-          {author}
-      </p>
+      <p>{author}</p>
     </a>
-  )
+  );
 }
