ab2873d184130f05e5b1f4721f54799992619afc
Jonathan Fielding <j.fielding@me.com>
2022-11-20 21:20:57 +0000

    fix: linting

--- components/compounds/budget-type-card/index.tsx
+++ components/compounds/budget-type-card/index.tsx
@@ -5,23 +5,30 @@ const navigation = [
   { name: 'Performance Resources', href: '/resources' },
 ]
 
+interface BudgetCardProps {
+  title: string;
+  description: string;
+  onBudgetClick: () => void;
+  selected: boolean;
+}
+
 export default function BudgetCard({
-  title, description, onBudgetClick, selected
-}) {
+  title,
+  description,
+  onBudgetClick,
+  selected,
+}: BudgetCardProps) {
   let classes = 'p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700 text-left';
   if (selected) {
-    classes = `${classes} shadow-inner text-indigo-600`
-  } else  {
-    classes = `${classes} hover:bg-slate-100 hover:shadow-md`
+    classes = `${classes} shadow-inner text-indigo-600`;
+  } else {
+    classes = `${classes} hover:bg-slate-100 hover:shadow-md`;
   }
-
 
   return (
     <button onClick={onBudgetClick} className={classes}>
       <h2 className="font-semibold text-2xl">{title}</h2>
-      <p>
-          {description}
-      </p>
+      <p>{description}</p>
     </button>
-  )
+  );
 }
